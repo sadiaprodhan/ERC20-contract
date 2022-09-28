854fda7780517cd390ea52544ca67ac8121e6282
Sadia Zahin Prodhan <55645529+sadiaprodhan@users.noreply.github.com>
2022-09-28 13:05:36 +0600

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 const ROPSTEN_URL = "https://ropsten.infura.io/v3/ce5d6e71eace484fa17a1d1f656bb15d";
 const RINKEBY_URL = "https://rinkeby.infura.io/v3/ce5d6e71eace484fa17a1d1f656bb15d";
-const PRIVATE_KEY = "12a6316d56e45a10a4ee13954c98eebf6c3d37615211d713abb44f664dded68b";
+const PRIVATE_KEY = #########
 const ETHERSCAN_API_KEY = "19H1YIAC2VY617HMG6W55W71RWQB5B1UPX";
 
 const config: HardhatUserConfig = {
